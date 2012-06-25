f248806dc45023fd8ede91a382fd03707b21dc13
Tobias Koppers <tobias.koppers@googlemail.com>
2012-06-25 13:05:22 +0200

    added lazy bundle loader

--- README.md
+++ README.md
@@ -10,6 +10,12 @@ require("bundle!./file.js")(function(file) {
 // wraps the require in a requre.ensure block
 ```
 
+The file is requested when you require the bundle loader. If you want it to request it lazy, use:
+
+``` javascript
+require("bundle/lazy!.file.js")
+```
+
 ## License
 
 MIT (http://www.opensource.org/licenses/mit-license.php)
---
+++ lazy.js
@@ -0,0 +1,29 @@
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author Tobias Koppers @sokra
+*/
+module.exports = function() {
+	this.cacheable && this.cacheable();
+	this.clearDependencies && this.clearDependencies();
+	var loaderSign = this.request.indexOf("!");
+	var requireRequest = this.request.substr(loaderSign); // including leading "!"
+	var result = [
+		"module.exports = function(cb) {\n",
+		"	require.ensure([], function(require) {\n",
+		"		cb(require(", null, "));\n",
+		"	});\n",
+		"}"];
+	result[3] = JSON.stringify(requireRequest);
+	return result.join("");
+}
+
+/*
+Output format:
+
+	module.exports = function(cb) {
+		require.ensure([], function(require) {
+			cb(require("xxx"));
+		});
+	}
+
+*/

502ec39e4b195a6487579d5d271340138dc709b1
Tobias Koppers <tobias.koppers@googlemail.com>
2012-11-02 10:08:41 +0100

    use query arguments

--- index.js
+++ index.js
@@ -2,35 +2,52 @@
 	MIT License http://www.opensource.org/licenses/mit-license.php
 	Author Tobias Koppers @sokra
 */
+var loaderUtils = require("loader-utils");
 module.exports = function() {
 	this.cacheable && this.cacheable();
 	this.clearDependencies && this.clearDependencies();
-	var loaderSign = this.request.indexOf("!");
-	var requireRequest = this.request.substr(loaderSign); // including leading "!"
-	var result = [
-		"var cbs = [], \n",
-		"	data;\n",
-		"module.exports = function(cb) {\n",
-		"	if(cbs) cbs.push(cb);\n",
-		"	else cb(data);\n",
-		"}\n",
-		"require.ensure([], function(require) {\n",
-		"	data = require(", null, ");\n",
-		"	var callbacks = cbs;\n",
-		"	cbs = null;\n",
-		"	for(var i = 0, l = callbacks.length; i < l; i++) {\n",
-		"		callbacks[i](data);\n",
-		"	}\n",
-		"});"];
-	result[8] = JSON.stringify(requireRequest);
+	var requireRequest = this.request;
+	if(this.loaderType == "loader") {
+		requireRequest = this.currentLoaders.slice(this.loaderIndex+1).join("!") + "!" + this.filenames[0];
+	} else if(this.loaderType != "postLoader") {
+		throw new Error("bundle-loader do not work as pre loader");
+	}
+	var query = loaderUtils.parseQuery(this.query);
+	var result;
+	if(query.lazy) {
+		result = [
+			"module.exports = function(cb) {\n",
+			"	require.ensure([], function(require) {\n",
+			"		cb(require(", null, "));\n",
+			"	});\n",
+			"}"];
+		result[3] = JSON.stringify(requireRequest);
+	} else {
+		result = [
+			"var cbs = [], \n",
+			"	data;\n",
+			"module.exports = function(cb) {\n",
+			"	if(cbs) cbs.push(cb);\n",
+			"	else cb(data);\n",
+			"}\n",
+			"require.ensure([], function(require) {\n",
+			"	data = require(", null, ");\n",
+			"	var callbacks = cbs;\n",
+			"	cbs = null;\n",
+			"	for(var i = 0, l = callbacks.length; i < l; i++) {\n",
+			"		callbacks[i](data);\n",
+			"	}\n",
+			"});"];
+		result[8] = JSON.stringify(requireRequest);
+	}
 	return result.join("");
 }
-module.exports.seperable = true;
+module.exports.separable = true;
 
 /*
 Output format:
 
-	var cbs = [], 
+	var cbs = [],
 		data;
 	module.exports = function(cb) {
 		if(cbs) cbs.push(cb);
